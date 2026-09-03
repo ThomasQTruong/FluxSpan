dfc9a6a54a6d20b053bd68920c82ad9f16257f57
Thomas Truong <TruongThomasQ@gmail.com>
2026-09-03 16:32:17 -0700

    style(ui): configure global base styles and Poppins font

--- fluxspan-ui/src/components/Navbar.tsx
+++ fluxspan-ui/src/components/Navbar.tsx
@@ -2,12 +2,12 @@ function Navbar() {
   return (
     <nav className="bg-blue-200 h-20 px-12 md:px-18 lg:px-24 flex justify-between items-center">
       {/* Logo */}
-      <div className="text-4xl font-bold text-left text-slate-900">
-        FluxSpan
+      <div className="text-4xl font-semibold text-left text-slate-900">
+        Flux&lt;Span&gt;
       </div>
       {/* Navigation Links */}
       <div className="flex items-center gap-4">
-        <a href="/login" className="text-2xl font-medium text-slate-900 hover:text-slate-700 cursor-pointer font-bold">
+        <a href="/login" className="text-2xl font-medium text-slate-900 hover:text-slate-700 cursor-pointer font-semibold">
           Log In
         </a>
       </div>
