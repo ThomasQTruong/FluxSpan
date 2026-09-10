2c482a3156ec152265319b01cd8c20d4aab1a8c6
Thomas Truong <TruongThomasQ@gmail.com>
2026-09-10 04:32:58 -0700

    feat(ui): add test backgrounds

--- fluxspan-ui/src/App.tsx
+++ fluxspan-ui/src/App.tsx
@@ -1,12 +1,15 @@
 import DashboardGrid from './components/DashboardGrid';
 import Navbar from './components/Navbar';
+import Beams from './components/backgrounds/Beams';
 
 function App() {
   return (
-    <div className="bg-slate-50 min-h-screen">
+    <Beams>
+      { /* <div className="bg-slate-50 min-h-screen bg-[radial-gradient(var(--color-slate-300)_1px,transparent_1px)] [background-size:24px_24px]"> */}
       <Navbar />
       <DashboardGrid />
-    </div>
+      { /* </div> */}
+    </Beams>
   );
 }
 
