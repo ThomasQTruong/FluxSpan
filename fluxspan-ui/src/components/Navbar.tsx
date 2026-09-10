3ecd0c7c3c7fecab3b11b4177e8dabefc4368c63
Thomas Truong <TruongThomasQ@gmail.com>
2026-09-10 05:12:24 -0700

    feat(ui): remove nav bar background color

--- fluxspan-ui/src/components/Navbar.tsx
+++ fluxspan-ui/src/components/Navbar.tsx
@@ -2,14 +2,15 @@ import Logo from './Logo';
 
 function Navbar() {
   return (
-    <nav className="bg-blue-200 h-20 px-12 md:px-18 lg:px-24 flex justify-between items-center">
+    <nav className="h-20 px-12 md:px-18 lg:px-24 flex justify-between items-center">
       {/* Logo */}
       <a href="/">
         <Logo />
       </a>
       {/* Navigation Links */}
       <div className="flex items-center gap-4">
-        <a href="/login" className="text-2xl font-medium text-slate-900 hover:text-slate-700 cursor-pointer font-semibold">
+        <a href="/login" className="text-2xl font-medium text-slate-900 cursor-pointer font-semibold
+                                    hover:text-slate-700">
           Log In
         </a>
       </div>
