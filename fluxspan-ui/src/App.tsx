16febda5e2c749efac07f59b05e06c40030fbdd7
Thomas Truong <TruongThomasQ@gmail.com>
2026-09-16 17:50:54 -0700

    feat(ui): improve DashboardGrid's responsiveness

--- fluxspan-ui/src/App.tsx
+++ fluxspan-ui/src/App.tsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <DottyBeams className="bg-slate-300">
       <Navbar />
-      <DashboardGrid className="h-216 px-8"></DashboardGrid>
+        <div className="w-full flex flex-col h-screen lg:h-[calc(100dvh-5rem)]">
+          <DashboardGrid className="pb-8 px-8 h-512 lg:flex-1"></DashboardGrid>
+      </div>
     </DottyBeams>
   );
 }
