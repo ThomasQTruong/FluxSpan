ae610d11f230a2f7423a5ae0caa64f59c00fa288
Thomas Truong <TruongThomasQ@gmail.com>
2026-09-21 23:24:21 -0700

    feat(auth): implement login UI

--- fluxspan-ui/src/components/auth/AuthForm.tsx
+++ fluxspan-ui/src/components/auth/AuthForm.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/lib/utils";
+import { SocialAuth } from "@/components/auth/SocialAuth";
+import { Link } from "react-router-dom";
 
 interface AuthFormProps {
   className?: string;
@@ -6,12 +8,31 @@ interface AuthFormProps {
 
 export function AuthForm({className}: AuthFormProps) {
   return (
-    <div className={cn("w-full h-[calc(100dvh-5rem)] md:h-[calc(100dvh-10rem)] flex flex-col justify-center items-center md:py-30", className)}>
-      <form className="w-screen max-w-2xl h-screen flex flex-col justify-center items-center gap-8 p-12 bg-rose-100 md:rounded-4xl">
-        <input type="text" placeholder="Email" className="w-full border rounded-md p-2" />
-        <input type="password" placeholder="Password" className="w-full border rounded-md p-2" />
-        <button type="submit" className="w-1/2 md:w-1/3 border rounded-4xl p-2">Submit</button>
-      </form>
+    <div className={cn("w-full min-h-[calc(100dvh-5rem)] flex flex-col",
+                       "justify-center items-center p-2 pb-22", className)}>
+      <div className="w-full max-w-2xl min-h-120 h-auto sm:min-h-150 flex flex-col
+                      py-12 justify-center items-center rounded-4xl bg-gray-50 shadow-md">
+        <form className="w-full flex flex-col gap-4 px-12 justify-center items-center">
+          <div className="w-full flex flex-col gap-2">
+            <input type="text" placeholder="Email"
+                   className="w-full border rounded-md p-2 hover:border-gray-700 hover:bg-gray-100
+                              transition-colors duration-300 ease-in-out" />
+            <input type="password" placeholder="Password"
+                   className="w-full border rounded-md p-2 hover:border-gray-700 hover:bg-gray-100
+                             transition-colors duration-300 ease-in-out" />
+          </div>
+          <button type="submit" className="w-full rounded-4xl bg-cyan-500/60 hover:bg-cyan-500/75
+                                    transition-colors duration-300 ease-in-out p-2">Submit</button>
+        </form>
+        <SocialAuth />
+        <div className="w-full">
+          <hr className="border-gray-300 m-2 md:m-4" />
+          <div className="w-full flex flex-row justify-center items-center gap-1">
+            <span className="text-sm">Don't have an account?</span>
+            <Link to="./" className="text-sm underline text-cyan-500">Sign Up instead.</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- fluxspan-ui/src/components/auth/SocialAuth.tsx
+++ fluxspan-ui/src/components/auth/SocialAuth.tsx
@@ -1,6 +1,12 @@
 export function SocialAuth() {
   return (
-    <div>
+    <div className="w-full pt-4">
+      <div className="w-full flex flex-col justify-center items-center">
+        <span className="font-bold text-sm">Or Sign in With:</span>
+        <div className="flex flex-row">
+          [ Google ]
+        </div>
+      </div>
     </div>
   );
 }
